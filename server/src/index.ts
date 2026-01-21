7ef187cf4d5df35baf021d05eab0944258d5e0ec
Reno-mathews <renomathews10pro@gmail.com>
2026-01-21 18:08:40 +0530

    Added more

---
+++ server/src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import todoRoutes from "./routes/todoRoutes";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/todos", todoRoutes);
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("MongoDB connected");
+        app.listen(5000, () => console.log("Server running on port 5000"));
+    })
+    .catch(err => console.error(err));
